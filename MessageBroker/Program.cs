022df835fe07aad0b19e7a075f89ef1b3a2a5bd1
sAndrin3 <sandrinemalanga2001@gmail.com>
2026-09-14 13:28:46 +0300

    Foundational work.

---
+++ MessageBroker/Program.cs
@@ -0,0 +1,22 @@
+using MessageBroker.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=MessageBroker.db"));
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
