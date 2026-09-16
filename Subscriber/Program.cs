5104c3c0617cd3ea9e7a332830f6f4ca1d2b2f93
sAndrin3 <sandrinemalanga2001@gmail.com>
2026-09-16 11:27:02 +0300

    Subscriber

---
+++ Subscriber/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http.Json;
+using Subscriber.Dtos;
+
+Console.WriteLine("Press ESC to stop");
+do
+{
+    HttpClient client = new HttpClient();
+    Console.WriteLine("Listening...");
+    while (!Console.KeyAvailable)
+    {
+        List<int> ackIds = await GetMessagesAsync(client);
+        
+        Thread.Sleep(2000);
+
+        if (ackIds.Count > 0)
+        {
+            await AckMessagesAsync(client, ackIds);
+        }
+    }
+} while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+static async Task<List<int>> GetMessagesAsync(HttpClient httpClient)
+{
+    List<int> ackIds = new List<int>();
+    List<MessageReadDto>? newMessages = new List<MessageReadDto>();
+
+    try
+    {
+        newMessages =
+            await httpClient.GetFromJsonAsync<List<MessageReadDto>>(
+                "http://localhost:5289/api/subscriptions/2/messages");
+    }
+    catch
+    {
+        return ackIds;
+    }
+
+    foreach (MessageReadDto msg in newMessages!)
+    {
+        Console.WriteLine($"{msg.Id} - {msg.TopicMessage} - {msg.MessageStatus}");
+        ackIds.Add(msg.Id);
+    }
+    return ackIds;
+}
+
+static async Task AckMessagesAsync(HttpClient httpClient, List<int> ackIds)
+{
+    var response = await httpClient.GetAsync("http://localhost:5289/api/subscriptions/2/messages");
+    var returnMessage = await response.Content.ReadAsStringAsync();
+    
+    Console.WriteLine(returnMessage);
+}
